23b220d93f61d33fe6347292264a440caf8251c4
Giovanni <gio.fon@gmail.com>
2017-01-03 16:18:16 +0100

    finish

--- index.android.js
+++ index.android.js
@@ -9,22 +9,41 @@ import {
 import MapView  from 'react-native-maps';
 
 export default class weather extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      pin: {
+        latitude: 0,
+        longitude: 0
+      }
+    }
+  }
+
   render() {
-    let pins ={
-      latitude: 37,
-      longitude: -95
-    };
 
     return (
       <MapView
-       style={styles.map} >
+       style={styles.map} 
+       onRegionChangeComplete={this.onRegionChangeComplete.bind(this)}
+       >
         <MapView.Marker
-          coordinate={pins}
+          coordinate={this.state.pin}
         />
       </MapView>
      );
   }
+
+  onRegionChangeComplete(region){
+    this.setState({
+      pin: {
+        longitude: region.longitude,
+        latitude: region.latitude
+      }
+    })
+  }
 }
+
 
 const styles = StyleSheet.create({
   map: {
